ccaeae1a57230380dcc93cbe0fcdb407a10498aa
Divyansh Sharma <divyansh9595@gmail.com>
2017-06-17 18:11:04 +0530

    basic twitter app

--- app.rb
+++ app.rb
@@ -7,6 +7,22 @@ class User
 	property :email, String
 	property :password, String
 end
+class Tweet
+	include DataMapper::Resource
+	property :id, Serial
+	property :email, String
+	property :text, String
+	property :like, Integer
+	property :unlike, Integer
+	property :time, DateTime
+end
+class Comment
+    include DataMapper::Resource
+    property :id, Serial
+    property :text, String
+    property :commentweet, Integer
+    property :commentor, String
+end
 DataMapper.finalize
 DataMapper.auto_upgrade!
 enable :sessions
@@ -56,4 +72,50 @@ post '/signup' do
 		session[:user_id]=user.id
 		return redirect '/'
 	end
+end
+post '/tweet' do
+    email=User.get(session[:user_id]).email
+    tweet=Tweet.new
+    tweet.email=email
+    tweet.text=params["text"]
+    tweet.like=0
+    tweet.unlike=0
+    tweet.time=Time.now
+    tweet.save
+    return redirect '/'
+end
+post '/like' do
+t=params["like"].to_i
+tweet=Tweet.get(t)
+tweet.like=tweet.like+1
+tweet.save
+return redirect '/'
+
+end
+post '/unlike' do
+	 t=params["unlike"].to_i
+     tweet=Tweet.get(t)
+     tweet.unlike=tweet.unlike+1
+     tweet.save
+	return redirect '/'
+end
+id=0
+get '/getcomment' do
+	erb :comment, locals:{tweet_id:id}
+end
+post '/comment' do
+	 id=params["comment"].to_i
+	 return redirect '/getcomment'
+end
+post '/postcomment' do
+	puts "helloworld"
+     text=params["comment"]
+     commentor=User.get(session[:user_id]).email
+     commentweet=params["comment_id"].to_i
+     comment=Comment.new
+     comment.commentor=commentor
+     comment.text=text
+     comment.commentweet=commentweet
+     comment.save
+     return redirect '/getcomment'
 end
